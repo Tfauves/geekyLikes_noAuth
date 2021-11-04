04551004defd3d87d1470cbf174bdc0a542ac618
Tfauves <tfauvelle@gmail.com>
2021-11-04 19:46:59 -0400

    putMapping.

--- src/main/java/com/careerdevs/geekylikes/controllers/DeveloperController.java
+++ src/main/java/com/careerdevs/geekylikes/controllers/DeveloperController.java
@@ -31,5 +31,23 @@ public class DeveloperController {
         return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
+    @PutMapping("/{id}")
+    public @ResponseBody Developer updateEmployee(@PathVariable Long id, @RequestBody Developer updates) {
+        Developer developer = repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        if (updates.getName() != null) developer.setName(updates.getName());
+        if (updates.getEmail() != null) developer.setEmail(updates.getEmail());
+        if (updates.getCohort() != null) developer.setCohort(updates.getCohort());
+        if (updates.getLanguages() != null) developer.setLanguages(updates.getLanguages());
+
+        return repository.save(developer);
+    }
+
+//    @DeleteMapping("/{id}")
+//    public ResponseEnity<String> destroyDev(@PathVariable Long id) {
+//        resp
+//
+//    }
+
 
 }
