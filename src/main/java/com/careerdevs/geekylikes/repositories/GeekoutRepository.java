39c099a46515884dae20c01d056f22797f129479
Tfauves <tfauvelle@gmail.com>
2021-11-09 12:34:44 -0500

    geekout posts.

---
+++ src/main/java/com/careerdevs/geekylikes/repositories/GeekoutRepository.java
@@ -0,0 +1,8 @@
+package com.careerdevs.geekylikes.repositories;
+
+import com.careerdevs.geekylikes.models.Geekout;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GeekoutRepository extends JpaRepository<Geekout, Long> {
+
+}
