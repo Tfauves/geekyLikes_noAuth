e7193d4928932f1caaead565ebc48b784dd9e461
Tfauves <tfauvelle@gmail.com>
2021-11-09 11:46:27 -0500

    geekout defined

--- src/main/java/com/careerdevs/geekylikes/models/Geekout.java
+++ src/main/java/com/careerdevs/geekylikes/models/Geekout.java
@@ -9,11 +9,47 @@ public class Geekout {
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "developer_id")
+    @JoinColumn(name = "developer_id", referencedColumnName = "id")
     private Developer developer;
-
-
 
     private String title;
     private String content;
+
+    public Geekout(Developer developer, String title, String content) {
+        this.developer = developer;
+        this. title = title;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Developer getDeveloper() {
+        return developer;
+    }
+
+    public void setDeveloper(Developer developer) {
+        this.developer = developer;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
